eac95daee43c10098e6e752c65c957461fa0c746
“wangyingwei” <wangyingwei@github.com>
2019-02-26 17:59:56 +0800

    update

--- config-client/src/main/java/com/wyw/configclient/controller/ConfigClientController.java
+++ config-client/src/main/java/com/wyw/configclient/controller/ConfigClientController.java
@@ -1,7 +1,6 @@
 package com.wyw.configclient.controller;
 
 
-import com.wyw.configclient.config.ConfigInfoProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
-
-    @Autowired
-    private ConfigInfoProperties configInfoValue;
-
-    @GetMapping("/configInfo")
-    public String configInfo(){
-        return configInfoValue.getConfig();
-    }
+//
+//    @Autowired
+//    private ConfigInfoProperties configInfoValue;
+//
+//    @GetMapping("/configInfo")
+//    public String configInfo(){
+//        return configInfoValue.getConfig();
+//    }
 }
